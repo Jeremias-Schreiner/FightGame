6298c56fb59a2360504d7a6f0300843fd80cac2b
Jeremias Schreiner <jeremias-ush@hotmail.com>
2022-05-26 13:16:34 -0300

    prueba de golpe y perdida de vida

--- main.js
+++ main.js
@@ -172,6 +172,7 @@ function animate(){
     ){
        player.isAttacking = false;
        console.log('ataque jugador')
+       document.querySelector('#enemyHealth').style.width = '20%'
     }
     if ( 
         rectangularCollision({
